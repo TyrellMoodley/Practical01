73da7e24c82bea5b59e8a1d991bc86b3200dfc40
tyrel <tyrel@LAPTOP-NEPM3VU5>
2025-03-02 18:40:31 +0200

    Add project files.

---
+++ TyrellStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TyrellStudyGroup.Models;
+
+namespace TyrellStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> Students = new List<Student>
+            {
+                new Student { StudentNumber = "u23540819", Name = "Tyrell", Surname = "Moodley", EmailAddress = "u23540819@tuks.co.za"},
+                new Student { StudentNumber = "u23843022", Name = "Newo", Surname = "Munyai", EmailAddress = "u23843022@tuks.co.za"},
+                new Student { StudentNumber = "u23613875", Name = "Kael", Surname = "Stainbank", EmailAddress = "u23613875@tuks.co.za"},
+                new Student { StudentNumber = "u23671904", Name = "Morobi", Surname = "Bonyana", EmailAddress = "u23671904tuks.co.za"},
+                new Student { StudentNumber = "u23638746", Name = "Declan", Surname = "Hart", EmailAddress = "u23638746@tuks.co.za"}
+            };
+            return View(Students);
+        }
+    }
+}
---
+++ TyrellStudyGroup/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TyrellStudyGroup.Models
+{
+    public class Student
+    {
+        public string StudentNumbers;
+        public string Name;
+        public string Surname;
+        public string EmailAddress;
+
+        public string StudentNumber
+        {
+            get { return StudentNumbers; }
+            set { StudentNumbers = value; }
+        }
+
+        public string studentName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public string studentSurname
+        {
+            get { return Surname; }
+            set { Surname = value; }
+        }
+
+        public string studentEmailAdress
+        {
+            get { return EmailAddress; }
+            set { EmailAddress = value; }
+        }
+    }
+}
